305b9a1d3accd2731c67cd152a6d13708f4bdf2b
cengo14 <cengizaydemir@live.com>
2024-10-01 22:58:11 +0300

    güncellendi

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -81,76 +81,73 @@ const ChatPage = ({ room, setRoom }) => {
   }, [messages]);
   return (
     <div>
-      <img className="background-wrap" src={background} alt="" />
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="chat-container">
-          <header>
-            <button className="btn-change" onClick={() => setRoom(null)}>
-              <GrFormPrevious className="prev-icon" size={36} />
-            </button>
-            <p className="room-name">{room}</p>
+      <div className="chat-container">
+        <header>
+          <button className="btn-change" onClick={() => setRoom(null)}>
+            <GrFormPrevious className="prev-icon" size={36} />
+          </button>
+          <p className="room-name">{room}</p>
+
+          <div>
+            {auth.currentUser ? (
+              <img
+                src={auth.currentUser?.photoURL}
+                className="person-photo"
+              ></img>
+            ) : (
+              <p className="person-name">
+                {auth.currentUser?.displayName?.charAt(0)}
+              </p>
+            )}
+          </div>
+        </header>
+        <main>
+          <div ref={lastMsg} />
+          {messages?.map((data, key) => (
+            <Msg data={data} key={key} />
+          ))}
+        </main>
+        <div className="chat-form">
+          <form onSubmit={handleSubmit}>
+            <input
+              value={textMsg}
+              onChange={(e) => setTextMsg(e.target.value)}
+              type="text"
+              placeholder="Mesaj yaz.."
+            />
+            <label className="file-label" htmlFor="file">
+              <GrAttachment size={30} />
+            </label>
+            <input
+              id="file"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
 
             <div>
-              {auth.currentUser ? (
-                <img
-                  src={auth.currentUser?.photoURL}
-                  className="person-photo"
-                ></img>
-              ) : (
-                <p className="person-name">
-                  {auth.currentUser?.displayName?.charAt(0)}
-                </p>
-              )}
-            </div>
-          </header>
-          <main>
-            <div ref={lastMsg} />
-            {messages?.map((data, key) => (
-              <Msg data={data} key={key} />
-            ))}
-          </main>
-          <div className="chat-form">
-            <form onSubmit={handleSubmit}>
-              <input
-                value={textMsg}
-                onChange={(e) => setTextMsg(e.target.value)}
-                type="text"
-                placeholder="Mesaj yaz.."
+              <EmojiPicker
+                onEmojiClick={(e) => setTextMsg(textMsg + e.emoji)}
+                skinTonePickerLocation="PREVIEW"
+                width={300}
+                height={400}
+                className="emoji"
+                open={emoji}
               />
-              <label className="file-label" htmlFor="file">
-                <GrAttachment size={30} />
-              </label>
-              <input
-                id="file"
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-              />
-
-              <div>
-                <EmojiPicker
-                  onEmojiClick={(e) => setTextMsg(textMsg + e.emoji)}
-                  skinTonePickerLocation="PREVIEW"
-                  width={300}
-                  height={400}
-                  className="emoji"
-                  open={emoji}
-                />
-                <button
-                  className="btn-msg"
-                  onClick={() => setEmoji(!emoji)}
-                  type="button"
-                >
-                  <MdOutlineEmojiEmotions size={32} />
-                </button>
-              </div>
-              <button className="btn-msg" type="submit">
-                <IoMdSend size={32} />
+              <button
+                className="btn-msg"
+                onClick={() => setEmoji(!emoji)}
+                type="button"
+              >
+                <MdOutlineEmojiEmotions size={32} />
               </button>
-            </form>
-          </div>
+            </div>
+            <button className="btn-msg" type="submit">
+              <IoMdSend size={32} />
+            </button>
+          </form>
         </div>
-      </Suspense>
+      </div>
     </div>
   );
 };
