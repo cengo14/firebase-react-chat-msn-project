db39654df867cba228e3fc76ac79b195f25d4a77
cengo14 <cengizaydemir@live.com>
2024-10-18 21:24:52 +0300

    güncellendi

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAz-UfD8yuA1kWn72ChWvv9J20kSNflZtM",
-  authDomain: "chatai-base.firebaseapp.com",
-  projectId: "chatai-base",
-  storageBucket: "chatai-base.appspot.com",
-  messagingSenderId: "207407752674",
-  appId: "1:207407752674:web:c393240ad867ff4e22c5bb",
-  measurementId: "G-LFQB3TZSG9",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
